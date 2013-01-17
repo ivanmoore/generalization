8accfdd69d7735eb4cebac7fe77a37a45d616776
Ivan Moore <ivan@teamoptimization.com>
2013-01-17 15:47:18 +0000

    put fields into list

--- AddEmployeeCmd.java
+++ AddEmployeeCmd.java
@@ -8,42 +8,37 @@
  */
 
 import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 public class AddEmployeeCmd {
     private static final byte[] commandChar = {0x02};
 
-    String name;
-    String address;
-    String city;
-    String state;
-    String yearlySalary;
+    List<String> fields = new ArrayList<String>();
 
     private int getSize() {
-        return CommandWriter.header.length +  CommandWriter.SIZE_LENGTH +  CommandWriter.CMD_BYTE_LENGTH + CommandWriter.footer.length +
-                name.getBytes().length + 1 +
-                address.getBytes().length + 1 +
-                city.getBytes().length + 1 +
-                state.getBytes().length + 1 +
-                yearlySalary.getBytes().length + 1;
+        int size = CommandWriter.header.length + CommandWriter.SIZE_LENGTH + CommandWriter.CMD_BYTE_LENGTH + CommandWriter.footer.length;
+        for (String field : fields) {
+            size = size + field.getBytes().length + 1;
+        }
+        return size;
     }
 
     public AddEmployeeCmd(String name, String address, String city, String state, int yearlySalary) {
-        this.name = name;
-        this.address = address;
-        this.city = city;
-        this.state = state;
-        this.yearlySalary = Integer.toString(yearlySalary);
+        fields.add(name);
+        fields.add(address);
+        fields.add(city);
+        fields.add(state);
+        fields.add(Integer.toString(yearlySalary));
     }
 
     public void write(OutputStream outputStream) throws Exception {
         outputStream.write(CommandWriter.header);
         outputStream.write(getSize());
         outputStream.write(commandChar);
-        CommandWriter.writeField(outputStream, name);
-        CommandWriter.writeField(outputStream, address);
-        CommandWriter.writeField(outputStream, city);
-        CommandWriter.writeField(outputStream, state);
-        CommandWriter.writeField(outputStream, yearlySalary);
+        for (String field : fields) {
+            CommandWriter.writeField(outputStream, field);
+        }
         outputStream.write(CommandWriter.footer);
     }
 }
